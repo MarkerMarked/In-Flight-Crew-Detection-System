c03aefc4e10625c32b7629e638c9d198ba3da32a
MarkerMarked <MarkerMarked@DESKTOP-895ST8U>
2018-09-09 22:15:48 -0400

    Updated GUI, Cleaned up code
    Inital Commit

--- src/airCrewPositioning/ECE485_GUI.java
+++ src/airCrewPositioning/ECE485_GUI.java
@@ -1,25 +1,18 @@
 package airCrewPositioning;
 
-import java.applet.Applet;
-import java.applet.AudioClip;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import javax.swing.ImageIcon;
+import java.awt.*;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-public class ECE485_GUI  implements Runnable  {
+
+
+public class ECE485_GUI {
     
     JFrame crewWindow = new JFrame("Crew Positions");
     JPanel crewPanel  = new JPanel();
     Graphics g;
     
-    public ECE485_GUI()
-    {
+    public ECE485_GUI() {
         
          crewWindow.getContentPane().add(crewPanel, BorderLayout.CENTER);
            crewPanel.setBackground(Color.black);
@@ -27,20 +20,9 @@ public class ECE485_GUI  implements Runnable  {
            crewWindow.setVisible(true);
            crewWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
            g = crewPanel.getGraphics();
-           Grids grid = new Grids(200, 1000, 40 , 8);
+           Grids grid = new Grids(240, 900, 11 , 3);
            grid.setVisible(true);
            crewPanel.add(grid, BorderLayout.CENTER);
-
-    }
-    
-    public void run() 
-    {
-        while(true)
-        {
-        //System.out.print("Working");
-        return;
-        }
-        
     }
     
     public static void main(String[] args) 
--- src/airCrewPositioning/Grids.java
+++ src/airCrewPositioning/Grids.java
@@ -1,6 +1,7 @@
 package airCrewPositioning;
 
 import java.awt.*;
+
 class Grids extends Canvas {
   int width, height, rows, columns;
   Grids(int w, int h, int r, int c) {
@@ -8,22 +9,25 @@ class Grids extends Canvas {
     rows = r;
     columns = c;
   }
+  
   public void paint(Graphics g) {
     int k;
     g.setColor(Color.green);
     width = getSize().width;
-    height = getSize().height ;
-    //horizontal lines
-    int htOfRow = height / (rows);
-    for (k = 0; k <= (rows); k++)
-      g.drawLine(0, k * htOfRow , width, (k * htOfRow));
-    //vertical Lines
-    int wdOfRow = width / (columns);
-    for (k = 0; k <= (columns+4); k++)
+    height = getSize().height;
+    
+    //Draw outside rectangle
+    g.drawRoundRect(1, 1, width-2, height-2, 25, 25);
+    
+    //Draw horizontal lines
+    int htOfRow = height / rows;
+    for (k = 1; k <= rows; k++)
+      g.drawLine(0, k*htOfRow , width, k*htOfRow);
+    
+    //Draw vertical lines
+    int wdOfRow = width / columns;
+    for (k = 1; k <= columns; k++)
       g.drawLine(k*wdOfRow , 0, k*wdOfRow , height);
     
-    
-    
   }
-
 }
