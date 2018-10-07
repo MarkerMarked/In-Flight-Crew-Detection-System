3ce11675ec4c59f86a3439b1e24d24671fa0e8e3
MarkerMarked <MarkerMarked@DESKTOP-895ST8U>
2018-10-07 01:06:08 -0400

    GUI and Multiple Member Update
    Updated GUI to properly show crew member circle object with no overlap
    on grid lines.
    Updated crew member object storage to associate with stored Tag IDs and
    store multiple crew member objects.

--- src/airCrewPositioning/CrewMember.java
+++ src/airCrewPositioning/CrewMember.java
@@ -3,22 +3,26 @@ package airCrewPositioning;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class CrewMember extends Canvas {
 	int identifier;
 	double widthFtLocation, heightFtLocation;
+	boolean needsDisplay;
 	
 	//Constructor - Takes ID, and location (from top left corner in ft.)
 	public CrewMember(int id, double wFt, double hFt) {
 		identifier = id;
 		widthFtLocation = wFt;
 		heightFtLocation = hFt;
+		needsDisplay = true;
 	}
 
 	//Update current location (from top left corner in ft.)
 	public void updateLocation(double wFt, double hFt) {
 		widthFtLocation = wFt;
 		heightFtLocation = hFt;
+		needsDisplay = true;
 	}
 	
 	public void paint(Graphics g) {
--- src/airCrewPositioning/ECE485_GUI.java
+++ src/airCrewPositioning/ECE485_GUI.java
@@ -1,19 +1,21 @@
 package airCrewPositioning;
 
 import java.awt.*;
-
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
-import javax.swing.JPanel;
 
 public class ECE485_GUI {
     
     JFrame crewWindow = new JFrame("Crew Positions");
-    JPanel crewPanel  = new JPanel();
     JLayeredPane crewPane = new JLayeredPane();
     Grids grid;
     
-    //CONSTANTS
+    //PARAMETERS
+    static int[] TAGIDS = new int[] {101,102};	//List of RFID tag TDs being used
+    static int NUMBEROFTAGS = 2;		//Number of RFID tags being used
+    
     static int HEIGHTFT = 30; 			// Height in Ft.
     static int WIDTHFT = 8;				// Width in Ft.
     static int SIZERATIO = 30;			// Ft. to Pix. Ratio
@@ -24,12 +26,20 @@ public class ECE485_GUI {
     static int GRIDWIDTH = WIDTHFT*SIZERATIO/COLUMNS;
     static int GRIDHEIGHT = HEIGHTFT*SIZERATIO/ROWS;
     
+    //Dictionary of Crew Member Tag IDs
+    Map<Integer, CrewMember> crewMembers = new Hashtable<Integer, CrewMember>();
+    
     public static void main(String[] args) 
     {
         ECE485_GUI crewPosition = new ECE485_GUI ();
     }
     
     private ECE485_GUI() {
+    	//Fill CrewMembers Dictionary with CM Objects
+    	for (int k = 0; k < NUMBEROFTAGS; k++) {
+    		crewMembers.put(TAGIDS[k], new CrewMember(TAGIDS[k],0.0,0.0));
+    	}
+    	
         crewWindow.getContentPane().add(crewPane, BorderLayout.CENTER);
         crewPane.setBackground(Color.black);
 	    crewWindow.setSize(DISPLAYWIDTH,DISPLAYHEIGHT);
@@ -38,17 +48,48 @@ public class ECE485_GUI {
 	    grid = new Grids(WIDTHFT*SIZERATIO, HEIGHTFT*SIZERATIO, ROWS, COLUMNS);
 	    crewPane.add(grid, 1);
 	    
-	    //Crew Member Testing Code - ALL DISTANCES FROM TOP LEFT CORNER
-        CrewMember firstCrewMember = new CrewMember(0, 6.0, 4.0);
-        displayCrewMember(firstCrewMember);
+	    //Create new data save file
+	    createNewSaveFile();
+	    
+crewMembers.get(101).updateLocation(6.0, 6.0);
+while (true) {	    
+	    //Update Location
+	
+
+	    
+	    //Display and Save Crew Member Location
+	    for (int k = 0; k < NUMBEROFTAGS; k++) {
+	        if (crewMembers.get(TAGIDS[k]).needsDisplay) {
+	        	displayCrewMember(crewMembers.get(TAGIDS[k]));
+	        	saveCrewMemberLocation(crewMembers.get(TAGIDS[k]));
+	        	crewMembers.get(TAGIDS[k]).needsDisplay = false;
+	        }
+	    }
+crewMembers.get(102).updateLocation(6.0, 8.0);	   
+crewMembers.get(101).updateLocation(2.0, 10.0);	  
+try {
+	TimeUnit.SECONDS.sleep(3);
+} catch (InterruptedException e) {
+	e.printStackTrace();
+}	    
+}
     }
+
     
     private void displayCrewMember(CrewMember crew) {
     	int convertedWidthLoc = (int) (crew.widthFtLocation * SIZERATIO);
     	int convertedHeightLoc = (int) (crew.heightFtLocation * SIZERATIO);
-    	crew.setSize(GRIDWIDTH, GRIDHEIGHT);
-    	crew.setLocation(convertedWidthLoc, convertedHeightLoc);
+    	crew.setSize(GRIDWIDTH - (GRIDWIDTH/6), GRIDHEIGHT - (GRIDHEIGHT/6));
+    	crew.setLocation(convertedWidthLoc + (GRIDWIDTH/12), convertedHeightLoc + (GRIDHEIGHT/12));
     	crewPane.add(crew, 0);
+    }
+    
+    private void createNewSaveFile() {
+    	
+    }
+    
+    private void saveCrewMemberLocation(CrewMember crew) {
+    	
     }
     
 }
